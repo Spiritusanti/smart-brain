9541906a8deb892d18e20fae69b930e9e3443170
Spiritusanti <mccrackenjacob@gmail.com>
2021-04-20 11:17:41 -0700

    added Modal to render profile component, added styling,  and linked into app.js state

--- src/components/profile/ProfileIcon.js
+++ src/components/profile/ProfileIcon.js
@@ -27,9 +27,9 @@ class ProfileIcon extends React.Component {
                     >
                         <img src="http://tachyons.io/img/logo.jpg" className="br-100 ba h3 w3 dib" alt="avatar" />        
                     </DropdownToggle>
-                    <DropdownMenu className='b--transparent shadow-5' style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
-                        <DropdownItem>View Profile</DropdownItem>
-                        <DropdownItem>Sign Out</DropdownItem>
+                    <DropdownMenu right className='b--transparent shadow-5' style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
+                        <DropdownItem onClick={this.props.toggleModal}>View Profile</DropdownItem>
+                        <DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
                     </DropdownMenu>
                 </Dropdown>
                 
