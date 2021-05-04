28a05a806db091b2177c5311895561f0249a16a3
Spiritusanti <mccrackenjacob@gmail.com>
2021-05-04 16:03:00 -0700

    working on adding session management to register route w/API

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -35,10 +35,14 @@ class Register extends React.Component {
     })
       .then(response => response.json())
       .then(user => {
+        console.log(user);
         if (user.id) {
           this.props.loadUser(user)
           this.props.onRouteChange('home');
         }
+      })
+      .then(session => {
+        console.log(session)
       })
   }
 
